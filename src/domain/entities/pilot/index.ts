e7b1d4bf69c897a23c33a3f03d2d65cad602f649
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-17 10:21:23 -0300

    feat: add pilot entity

---
+++ src/domain/entities/pilot/index.ts
@@ -0,0 +1,83 @@
+import Planets from '@/domain/enums/planet';
+import OperationResult from '@/shared/responses/operation-result';
+
+type CreatePilotData = {
+  id? : number;
+
+  name :string;
+
+  certification : string;
+
+  age : number;
+}
+
+class Pilot {
+  public readonly id : number;
+
+  public readonly name :string;
+
+  public readonly locationPlanet : Planets;
+
+  public readonly certification : string;
+
+  public readonly age : number;
+
+  private _credits : number;
+
+  get credits() : number {
+    return this._credits;
+  }
+
+  set credits(value : number) {
+    if (value < 0) {
+      this._credits = 0;
+
+      return;
+    }
+
+    this._credits = value;
+  }
+
+  private constructor(pilotData : CreatePilotData) {
+    this.id = pilotData.id ?? 0;
+    this.name = pilotData.name;
+    this.age = pilotData.age;
+    this.certification = pilotData.certification;
+    this.credits = 0;
+  }
+
+  public static create(pilotData : CreatePilotData) : OperationResult<Pilot> {
+    const result = new OperationResult<Pilot>(true);
+
+    const validCertification = this._validateCertification(pilotData.certification);
+    if (!validCertification) {
+      result.success = false;
+      result.message = 'Invalid pilot certification';
+
+      return result;
+    }
+
+    const validAge = this._validateAge(pilotData.age);
+    if (!validAge) {
+      result.success = false;
+      result.message = 'Pilot need to be older than 18';
+
+      return result;
+    }
+
+    result.data = new this(pilotData);
+
+    return result;
+  }
+
+  private static _validateCertification(pilotCertification : string) : boolean {
+    // TODO Validate properly
+    return pilotCertification.length === 7;
+  }
+
+  private static _validateAge(age: number) : boolean {
+    return age >= 18;
+  }
+}
+
+export default Pilot;
