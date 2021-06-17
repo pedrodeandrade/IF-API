192d22fe86ba3829b4f8a3d2ce6fd4276772de2d
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-17 10:22:11 -0300

    feat: add enum to define planets

---
+++ src/domain/enums/planet.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+enum Planets {
+  Andvari = 'Andvari',
+  Demeter = 'Demeter',
+  Aqua = 'Aqua',
+  Calas = 'Calas'
+}
+
+export default Planets;
