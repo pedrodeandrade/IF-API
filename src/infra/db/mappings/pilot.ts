9ddc65a3ecde62c670614542900130ba5e05cb9f
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-19 21:58:17 -0300

    chore: add pilot entity mapping

---
+++ src/infra/db/mappings/pilot.ts
@@ -0,0 +1,41 @@
+import { EntitySchema } from 'typeorm';
+
+import Pilot from '@/domain/entities/pilot';
+import Planets from '@/domain/enums/planet';
+
+const PilotMapping = new EntitySchema<Pilot>({
+  name: 'Pilot',
+  tableName: 'pilots',
+  columns: {
+    id: {
+      type: Number,
+      primary: true,
+      generated: true,
+    },
+    name: {
+      type: String,
+      nullable: false,
+    },
+    certification: {
+      type: String,
+      nullable: false,
+    },
+    credits: {
+      type: Number,
+      nullable: false,
+      default: 0,
+    },
+    age: {
+      type: Number,
+      nullable: false,
+    },
+    locationPlanet: {
+      name: 'location_planet',
+      type: String,
+      enum: Planets,
+      nullable: true,
+    },
+  },
+});
+
+export default PilotMapping;
