7f00e9110ebb413614a3ccf2378518575935acd8
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-19 21:57:16 -0300

    chore: add TypeOrm configs

---
+++ src/infra/db/config/index.ts
@@ -0,0 +1,29 @@
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
+
+// TODO Move dotenv to appropriate layer
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectionOptions : ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['src/infra/db/mappings/*.ts'],
+  migrations: ['src/infra/db/migrations/*.ts'],
+  logging: process.env.NODE_ENV === 'development' ? ['query', 'error'] : false,
+  logger: 'advanced-console',
+  synchronize: false,
+};
+
+/**
+ * Create TypeOrm connection with the database
+ */
+const typeOrmConnectionFactory = () : Promise<Connection> => createConnection(connectionOptions);
+
+export { typeOrmConnectionFactory };
+
+export default connectionOptions;
