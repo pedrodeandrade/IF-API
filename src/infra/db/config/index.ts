6508c19f7296b776dbcbea32b4bb71268f0a031d
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-20 13:00:49 -0300

    chore: change db connection options to use env object

--- src/infra/db/config/index.ts
+++ src/infra/db/config/index.ts
@@ -1,17 +1,14 @@
 import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 
-// TODO Move dotenv to appropriate layer
-import dotenv from 'dotenv';
-
-dotenv.config();
+import env from '@/shared/env';
 
 const connectionOptions : ConnectionOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: env.dbHost,
+  port: env.dbPort,
+  username: env.dbUser,
+  password: env.dbPassword,
+  database: env.dbName,
   entities: ['src/infra/db/mappings/*.ts'],
   migrations: ['src/infra/db/migrations/*.ts'],
   logging: process.env.NODE_ENV === 'development' ? ['query', 'error'] : false,
