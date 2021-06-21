76ac1bc49b84e24ca4de0cc175dd3ba383e6c7bb
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-21 17:03:35 -0300

    feat: add FuelShip controller

---
+++ src/presentation/controllers/ship/fuel-ship.ts
@@ -0,0 +1,31 @@
+import FuelShip from '@/domain/contracts/use-cases/ship/fuel-ship';
+import Controller from '@/presentation/contracts/controller';
+import { HttpResponse } from '@/presentation/contracts/http';
+import { badRequest, ok, serverError } from '@/presentation/utils/http';
+
+type FuelShipRequestData = {
+  shipId : number,
+  fuelAmount : number
+}
+
+class FuelShipController implements Controller<FuelShipRequestData> {
+  private readonly _useCase : FuelShip;
+
+  constructor(useCase: FuelShip) {
+    this._useCase = useCase;
+  }
+
+  async handle(request: FuelShipRequestData): Promise<HttpResponse> {
+    try {
+      await this._useCase.handle(request);
+
+      return ok('Ship fueled successfully');
+    } catch (error) {
+      if (error.name === 'BusinessError') { return badRequest(error.message); }
+
+      return serverError(error.message);
+    }
+  }
+}
+
+export default FuelShipController;
