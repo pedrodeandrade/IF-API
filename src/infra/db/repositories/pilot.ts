38521ac44cd475f4b9cb0cd11b32e28ef212cc78
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-06-21 16:42:30 -0300

    feat: add update method to pilot repository

--- src/infra/db/repositories/pilot.ts
+++ src/infra/db/repositories/pilot.ts
@@ -5,12 +5,17 @@ import {
 import Pilot from '@/domain/entities/pilot';
 import AddPilotRepository from '@/application/contracts/repositories/pilot/add-pilot-repository';
 import GetPilotRepository from '@/application/contracts/repositories/pilot/get-pilot-repository';
+import UpdatePilotRepository from '@/application/contracts/repositories/pilot/update-pilot-repository';
 
-class PilotRepository implements AddPilotRepository, GetPilotRepository {
+class PilotRepository implements AddPilotRepository, GetPilotRepository, UpdatePilotRepository {
   private readonly _ormRepository : Repository<Pilot>;
 
   public constructor(ormRepository : Repository<Pilot>) {
     this._ormRepository = ormRepository;
+  }
+
+  async update(pilot: Pilot): Promise<void> {
+    await this._ormRepository.save(pilot);
   }
 
   async add(pilot: Pilot): Promise<boolean> {
